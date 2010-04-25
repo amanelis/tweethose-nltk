bef99850c10ae30b4488ee9fa8ffd9a6ea134e68
Alex Manelis <amanelis@gmail.com>
2010-04-25 17:08:57 -0500

    1272230428

--- phirehose/sample.php
+++ phirehose/sample.php
@@ -4,12 +4,27 @@ require_once("hose/lib/Phirehose.php");
 class SampleConsumer extends Phirehose {
 
 	public function enqueueStatus($status) {
+		/*
 		$data = json_decode($status, true);
 		if(is_array($data) && isset($data['user']['screen_name'])) {
 			print $data['user']['screen_name'].': '.urldecode($data['text'])."<br />";
 		}
-	}
+		*/
+		$decode = json_decode($status, true);
+		if($decode['user']['lang'] == 'en'){
+			$user = $decode['user']['screen_name'];
+			$post = $decode['text'];
 
+			echo $user.": ".$post."<br />";
+		}
+
+
+		/*	
+		echo "<pre>";
+		print_r($decode);	
+		echo "</pre>";
+		*/
+	}
 }
 
 $sc = new SampleConsumer('zandermane', 'alex18257', Phirehose::METHOD_SAMPLE);
