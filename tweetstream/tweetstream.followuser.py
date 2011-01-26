2aafdc3406234f23a380d3d4bb34d6c5801e2363
Alex Manelis <amanelis@gmail.com>
2011-01-25 20:47:48 -0800

    removed password

--- tweetstream/tweetstream.followuser.py
+++ tweetstream/tweetstream.followuser.py
@@ -2,7 +2,7 @@
 import tweetstream
 
 users = [1, 454]
-stream = tweetstream.FollowStream("zandermane", "alex18257", users)
+stream = tweetstream.FollowStream("username", "password", users)
 
 for tweet in stream:
 	print "Got tweet from: ", tweet["user"]["screen_name"]
